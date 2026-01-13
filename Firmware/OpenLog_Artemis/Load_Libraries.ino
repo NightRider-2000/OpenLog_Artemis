a970b9392db5688e2433b9716e918f81cf912dc4
NightRider-2000 <sam.shartzer@gmail.com>
2026-01-12 20:53:16 -0500

    Loaded_Libraries_Notes_Only

---
+++ Firmware/OpenLog_Artemis/Load_Libraries.ino
@@ -0,0 +1,39 @@
+// SdFat by Bill Greiman (Tested version: 2.2.0): http://librarymanager/All#SdFat_exFAT
+// http://librarymanager/All#SparkFun_ICM_20948_IMU
+// http://librarymanager/All#SparkFun_I2C_Mux
+// http://librarymanager/All#SparkFun_CCS811
+// http://librarymanager/All#SparkFun_VL53L1X
+// http://librarymanager/All#SparkFun_BME280
+// http://librarymanager/All#SparkFun_LPS25HB
+// http://librarymanager/All#SparkFun_VEML6075
+// http://librarymanager/All#SparkFun_PHT_MS8607
+// http://librarymanager/All#SparkFun_MCP9600
+// http://librarymanager/All#SparkFun_SGP30
+// http://librarymanager/All#SparkFun_VCNL4040
+// http://librarymanager/All#SparkFun_MS5637
+// http://librarymanager/All#SparkFun_TMP102
+// http://librarymanager/All#SparkFun_TMP117
+// http://librarymanager/All#SparkFun_u-blox_GNSS
+// http://librarymanager/All#SparkFun_NAU7802
+// http://librarymanager/All#SparkFun_SCD30
+// http://librarymanager/All#Qwiic_Humidity_AHT20
+// http://librarymanager/All#SparkFun_SHTC3
+// http://librarymanager/All#SparkFun_ADS122C04
+// http://librarymanager/All#SparkFun_MicroPressure
+// http://librarymanager/All#SparkFun_Particle_Sensor_SN-GCJA5
+// http://librarymanager/All#SparkFun_SGP40
+// http://librarymanager/All#SparkFun_SDP3x
+// http://librarymanager/All#SparkFun_Qwiic_Button_Switch
+// http://librarymanager/All#SparkFun_Bio_Sensor
+// http://librarymanager/All#SparkFun_6DoF_ISM330DHCX
+// http://librarymanager/All#SparkFun_MMC5983MA
+// http://librarymanager/All#SparkFun_ADS1015
+// http://librarymanager/All#SparkFun_KX13X
+// http://librarymanager/All#SparkFun_LPS28DFW_Arduino_Library
+// http://librarymanager/All#SparkFun_VEML7700
+// http://librarymanager/All#SparkFun_TMP102
+
+
+// Issues Noted:
+// VEML7700 does not seem to load right, I commented it out in the firmware code since not needed.
+// There is comment about UART bug but seems like it would be fixed in Git.  There is a zip file, "UartPower3.zip" that with 6 or 7 files that can be copied if there is an issue. See end of creating binary help file for more details. 
